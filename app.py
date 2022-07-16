3b7b1afece6204073b36c36c8eca4919805f40b2
Alexandra Fren <35177685+alexandrafren@users.noreply.github.com>
2022-07-16 14:09:03 -0400

    add additional routes for new tables/templates

--- app.py
+++ app.py
@@ -7,7 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/bundles')
+def get_bundles():
+    return render_template("bundles.html")
 
+@app.route('/characters')
+def get_characters():
+    return render_template("characters.html")
+
+@app.route('/shops')
+def get_shops():
+    return render_template("shops.html")
+
+@app.route('/regions')
+def get_regions():
+    return render_template("regions.html")
+
+@app.route('/items')
+def get_items():
+    return render_template("items.html")
 
 
 if __name__ == '__main__':
