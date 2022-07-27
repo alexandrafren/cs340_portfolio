880d02e9aba0f88d39e34b680e9c7ff48614ea6a
Alexandra Fren <alexandra.fren@gmail.com>
2022-07-27 18:03:38 -0400

    implememnt CRUD routes in app.py

--- app.py
+++ app.py
@@ -11,13 +11,46 @@ def index():
 def get_bundles():
     return render_template("bundles.html")
 
+@app.route('/bundleitems')
+def get_bundleitems():
+    return render_template("bundleitems.html")
+
 @app.route('/characters')
 def get_characters():
     return render_template("characters.html")
 
+# Character Item CRUD Functions
+@app.route('/characteritems')
+def get_characteritems():
+    """ This function implements the Retrieve funciton of CRUD for Character Items"""
+    return render_template("characteritems.html")
+
+@app.route('/characteritems/create', methods = ['POST'])
+def create_characteritems():
+    """ This function implements the Create function of CRUD for Character Items """
+    return "Create Character Item"
+
+@app.route('/characteritems/update/<int:id>', methods = ['GET', 'POST'])
+def update_characteritems(id):
+    """ This function implements the Update function of CRUD for Character Items. """
+    # GET
+    return render_template('updatecharacteritems.html', item = id)
+    # POST - update item and render characteritems.html
+
+@app.route('/characteritems/delete/<int:id>', methods= ['GET', 'POST'])
+def delete_characteritems(id):
+    """ This function implements the Delete function of CRUD for Character Items. """
+    # GET
+    return render_template('deletecharacteritems.html', item = id)
+    # POST - delete item and render characteritems.html
+
 @app.route('/shops')
 def get_shops():
     return render_template("shops.html")
+
+@app.route('/shopitems')
+def get_shopitems():
+    return render_template("shopitems.html")
 
 @app.route('/regions')
 def get_regions():
